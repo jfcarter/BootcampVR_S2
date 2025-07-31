87bd2877a78b4970c50d9d4ae868f2e5db5513b7
Je'Don Carter <jedon007@yahoo.com>
2025-07-31 11:50:27 -0700

    works

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,16 +6,13 @@ public class BallController : MonoBehaviour
 {
     private float timer;
 
+    public Rigidbody rb;
     public float timeToDestroy = 30;
+    public float reboundForceMultiplier = 1.2f;
+
     public void DestroyBall()
     {
         Destroy(gameObject);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -27,6 +24,30 @@ public class BallController : MonoBehaviour
         {
             DestroyBall();
             timer = 0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name.Contains("Baseball Bat"))
+        {
+            Debug.Log("Hit baseball bat");
+            if (collision.contactCount > 0)
+            {
+                ContactPoint contact = collision.GetContact(0);
+
+                // Get the normal at the contact point
+                Vector3 normal = contact.normal;
+
+                // Reflect the current velocity off the normal
+                Vector3 reflectedVelocity = Vector3.Reflect(rb.velocity, normal);
+
+                // Optional: Apply a multiplier to simulate hitting it harder
+                rb.velocity = reflectedVelocity * reboundForceMultiplier;
+
+                Debug.DrawRay(contact.point, normal, Color.red, 1f);
+                Debug.Log("Reflected off bat! New velocity: " + rb.velocity);
+            }
         }
     }
 }
